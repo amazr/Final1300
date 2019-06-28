9901e89b45b839c199a4e62c61ced470f138452b
bouldercoder9 <alma9011@colorado.edu>
2019-06-28 01:21:47 -0600

    integer support added

--- Language.cpp
+++ Language.cpp
@@ -16,24 +16,29 @@ struct typelocation {
 	int location;
 };
 
+struct line {
+	string lineStr;
+	int lineNum;
+};
+
 
 //this function removes whitespace from a line and then returns the line
-string removeWhitespace(string line) {
-	for (int i = 0; i < line.size(); i++) {
-		if (line[i] == ' ') {
-			line.erase(line.begin() + i);
+line removeWhitespace(line thisLine) {
+	for (int i = 0; i < thisLine.lineStr.size(); i++) {
+		if (thisLine.lineStr[i] == ' ') {
+			thisLine.lineStr.erase(thisLine.lineStr.begin() + i);
 		}
 	}
-	return line;
+	return thisLine;
 }
 
 //this function returns typelocation struct with a new data type and a new location for its ending character
-typelocation searchForType(string line) {
+typelocation searchForType(line thisLine) {
 	typelocation newType;
 	for (auto word : datatypes) {
-		size_t found = line.find(word);
+		size_t found = thisLine.lineStr.find(word);
 		if (found != string::npos) {
-			cout << " ... keyword was found in: " << found;		//debuggin line
+			//cout << " ... keyword was found in: " << found;		//debuggin line
 			newType.type = word;
 			newType.location = found + (word.size() - 1);
 			return newType;
@@ -42,38 +47,77 @@ typelocation searchForType(string line) {
 
 	newType.location = -1;
 	return newType;
+}
 
+
+//This function gets a new variable name and stores it in the proper unordered map of that variables declared type
+void createNewVar(line thisLine, typelocation varType) {
+
+	size_t found = thisLine.lineStr.find("=");
+	string varName = "";
+	string varValueStr = "";
+
+	if (found != string::npos) {
+		//this finds the variable name
+		for (int i = varType.location + 1; i < found; i++) {
+			varName += thisLine.lineStr[i];
+		}
+		//this finds the variable value
+		for (int i = found + 1; i < thisLine.lineStr.size(); i++) {
+			varValueStr += thisLine.lineStr[i];
+		}
+	}
+
+	//Integer variable creation
+	if (varType.type == datatypes[0]) {
+		try {
+			int_vars[varName] = stoi(varValueStr);
+		}
+		catch (invalid_argument) {
+			cout << "Incorrect datatype entered on line: " << thisLine.lineNum << endl;
+		}
+		catch (out_of_range) {
+			cout << "Value out of range on line: " << thisLine.lineNum << endl;
+		}
+	}
 }
 
 //this function will interpret each line
-void readLine(string line) {
+void readLine(line thisLine) {
 	typelocation newType;
-	line = removeWhitespace(line); 
+	thisLine = removeWhitespace(thisLine);
 
-	cout << line;											//debugging line
+	//cout << thisLine.lineStr;											//debugging line
 	
 	//Find new datatype
-	newType = searchForType(line);
+	newType = searchForType(thisLine);
 	if (newType.location != -1) {
-		cout << " ... end location for type is: " << newType.location;
+		createNewVar(thisLine, newType);
 	}
 
-	cout << endl;											//debugging line
+	//cout << endl;											//debugging line
 }
 
 //this function opens the file to be read
 void openFile(string fileName) {
 	ifstream ifs(fileName);
 	
-	string line;
-	while (getline(ifs, line)) {
-		readLine(line);
+	line thisLine;
+	thisLine.lineNum = 1;
+	while (getline(ifs, thisLine.lineStr)) {
+		thisLine.lineNum++;
+		readLine(thisLine);
 	}
 }
 
 int main() {
 
 	openFile("ide.txt");
+
+	cout << endl << endl << "All vars listed below" << endl;
+	for (auto var : int_vars) {
+		cout << "var name: " << var.first << " = " << var.second << endl;
+	}
 
 	return 0;
 }
---
+++ README.txt
@@ -0,0 +1,19 @@
+HELLO!
+This is going to be a brief overview/guide for my UNNAMED language
+This language is currently interpreted by c++!
+
+BASIC SYNTAX RULES:
+	There is no end of line character. To make a distinction between lines, simply hit enter and start typing on the next line!
+	Whitespace will be completely ignored!
+
+Current Supported Datatypes:
+int (integer)
+
+Planned Supported Datatypes:
+dec (aka double/float)
+chr (character/char)
+str (string)
+
+HOW TO DECLARE A VARIABLE:
+	data_type var_name = value
+	
