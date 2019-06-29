e98f50e829ba48d6011b629576764ef109dba495
AlexLaptop <alma9011@colorado.edu>
2019-06-29 00:37:52 -0600

    Added support for appending int variables to string literals

--- Language.cpp
+++ Language.cpp
@@ -8,37 +8,123 @@ using namespace std;
 
 unordered_map<string, int> int_vars;
 vector<string> datatypes = { "int", "dec", "chr", "str" };
-vector<string> operands = {};
-vector<string> keywords = {};
+vector<string> operators = {"+", "-", "*", "/", "++", "--", "+=", "-=", "%"};
+vector<string> keywords = { "display" };
+
 
 struct typelocation {
 	string type;
-	int location;
+	int location;	//This is the location of the LAST char in the type name
 };
 
 struct line {
 	string lineStr;
+	string strLiteral;
 	int lineNum;
 };
 
+//This will display all characters to the right of "display:" to the terminal
+void displayFunction(line thisLine, int wordLocation) {
+	
+	if (thisLine.strLiteral == "") {
+		cout << endl << "Warning on line " << thisLine.lineNum << " -> Empty display function" << endl;
+		return;
+	}
+
+	cout << thisLine.strLiteral;
+	
+}
+
+//This function will check if a keyword was used and handle that keywords function
+void checkForKeywords(line thisLine) {
+	for (auto word : keywords) {
+		size_t found = thisLine.lineStr.find(word);
+		if (found != string::npos) {
+			if (word == keywords.at(0)) {
+				displayFunction(thisLine, found);
+			}
+		}
+	}
+}
+
+//This will create a string literal if a line has one. This is to exempt it from the clear whitespace rule
+line createStrLiteral(line thisLine) {
+	bool inLiteral = false;
+	bool gettingVar = false;
+	int varCounter = 0;
+	vector<string> vars;
+	string varName = "";
+
+
+	for (int i = 0; i < thisLine.lineStr.size(); i++) {
+
+		if (thisLine.lineStr[i] == '"') {
+			if (inLiteral) {
+				inLiteral = false;
+			}
+			else {
+				inLiteral = true;
+			}
+		}
+		if (inLiteral && thisLine.lineStr[i] != '\"') {
+			thisLine.strLiteral += thisLine.lineStr[i];
+		}
+
+		//Ending line statement is a '.'
+		if (!inLiteral && thisLine.lineStr[i] == '.') {
+			thisLine.strLiteral += "\n";
+		}
+
+		//Vars appended to strings must be surrounded by ':'
+		if (!inLiteral && thisLine.lineStr[i] == ':') {
+			if (!gettingVar) {
+				gettingVar = true;
+				varCounter++;
+
+			}
+			else {
+				gettingVar = false;
+				varName.erase(varName.begin());
+				vars.push_back(varName);
+				varName = "";
+				if (int_vars.find(vars.at(varCounter - 1)) != int_vars.end()) {
+					thisLine.strLiteral += to_string(int_vars[vars.at(varCounter - 1)]);;
+
+				}
+			}
+		}
+
+		if (gettingVar) {
+			varName += thisLine.lineStr[i];
+		}
+
+	}
+	
+	return thisLine;
+}
 
 //this function removes whitespace from a line and then returns the line
 line removeWhitespace(line thisLine) {
+	
+	//IMPORTANT: If this is not called, all string literals will have NO whitespace. It is important to use line. strLiteral when reffering to literals
+	thisLine = createStrLiteral(thisLine);
+
 	for (int i = 0; i < thisLine.lineStr.size(); i++) {
 		if (thisLine.lineStr[i] == ' ') {
 			thisLine.lineStr.erase(thisLine.lineStr.begin() + i);
 		}
 	}
+
 	return thisLine;
 }
 
 //this function returns typelocation struct with a new data type and a new location for its ending character
 typelocation searchForType(line thisLine) {
+
 	typelocation newType;
 	for (auto word : datatypes) {
 		size_t found = thisLine.lineStr.find(word);
 		if (found != string::npos) {
-			//cout << " ... keyword was found in: " << found;		//debuggin line
 			newType.type = word;
 			newType.location = found + (word.size() - 1);
 			return newType;
@@ -57,6 +143,7 @@ void createNewVar(line thisLine, typelocation varType) {
 	string varName = "";
 	string varValueStr = "";
 
+	//Runs if a variable is created with an equal sign and a value
 	if (found != string::npos) {
 		//this finds the variable name
 		for (int i = varType.location + 1; i < found; i++) {
@@ -67,14 +154,25 @@ void createNewVar(line thisLine, typelocation varType) {
 			varValueStr += thisLine.lineStr[i];
 		}
 	}
+	//Runs if variable value is not declared at this time
+	else {
+		for (int i = varType.location + 1; i < thisLine.lineStr.size(); i++) {
+			varName += thisLine.lineStr[i];
+		}
+	}
 
 	//Integer variable creation
 	if (varType.type == datatypes[0]) {
 		try {
-			int_vars[varName] = stoi(varValueStr);
+			if (varValueStr == "") {
+				int_vars[varName] = 0;	//If var is not declared set it to 0
+			}
+			else {
+				int_vars[varName] = stoi(varValueStr);
+			}
 		}
 		catch (invalid_argument) {
-			cout << "Incorrect datatype entered on line: " << thisLine.lineNum << endl;
+			cout << "Incorrect value: " << varValueStr << " entered on line: " << thisLine.lineNum << endl;
 		}
 		catch (out_of_range) {
 			cout << "Value out of range on line: " << thisLine.lineNum << endl;
@@ -85,17 +183,17 @@ void createNewVar(line thisLine, typelocation varType) {
 //this function will interpret each line
 void readLine(line thisLine) {
 	typelocation newType;
+
+	//This will also handle the spacing within string literals
 	thisLine = removeWhitespace(thisLine);
 
-	//cout << thisLine.lineStr;											//debugging line
-	
 	//Find new datatype
 	newType = searchForType(thisLine);
 	if (newType.location != -1) {
 		createNewVar(thisLine, newType);
 	}
 
-	//cout << endl;											//debugging line
+	checkForKeywords(thisLine);
 }
 
 //this function opens the file to be read
@@ -105,19 +203,22 @@ void openFile(string fileName) {
 	line thisLine;
 	thisLine.lineNum = 1;
 	while (getline(ifs, thisLine.lineStr)) {
-		thisLine.lineNum++;
 		readLine(thisLine);
+		thisLine.lineNum++;
 	}
 }
 
 int main() {
 
-	openFile("ide.txt");
+	string fileName = "";
+	cout << "Welcome to UNNAMED LANGUAGE v1" << endl << "Enter file name to run" << endl;
+	cin >> fileName;
+	openFile(fileName);
 
-	cout << endl << endl << "All vars listed below" << endl;
-	for (auto var : int_vars) {
-		cout << "var name: " << var.first << " = " << var.second << endl;
-	}
+	//cout << endl << endl << "All vars listed below" << endl;
+	//for (auto var : int_vars) {
+	//	cout << "var name: " << var.first << " = " << var.second << endl;
+	//}
 
 	return 0;
 }
--- README.txt
+++ README.txt
@@ -1,23 +1,32 @@
 HELLO!
-This is going to be a brief overview/guide for Completely Useful Programing (CUP)
+This is going to be a brief overview/guide for Completely Useful Programming (CUP)
 This language is currently interpreted by C++!
 
 HOW TO RUN:
-	Step 1: Save your CUP code in a file called "ide.txt"
+	Step 1: Save your file in the form of "fileName.cup"
 	Step 2: Compile and run Language.cpp
-	Step 3: Admire your freshly brewed CUP code!
+	Step 3: Input your amazing and perfectly done CUP codes file name WITH the extension when prompted!
+	Sted 4: Now get on with it!
 
 BASIC SYNTAX RULES:
 	There is no end of line character. To make a distinction between lines, simply hit enter and start typing on the next line!
-	Whitespace will be completely ignored!
+	Whitespace will be completely ignored EXCEPT inside string literals
+	When appending a value to a string literal wrap the variable name in ':'  EXAMPLE -> display "I have " :numDogs: " dogs" ->I have 4 dogs
 	
 DECLARING A VARIABLE: 
 	data_type var_name = var_value
 	currently a variable name can be whatever you'd like
 	if there is an error an exception with the line number will be thrown
 
+DISPLAYING TO TERMINAL:
+	display: "hello world"
+	Any '.' (period) outside of quotation marks will end the line. Equivalent to 'endl' or '\n'
+	If display is called but there is no argument a warning will be thrown but your code will still be interpreted
+
 CURRENT KEYWORDS:
-int 
+int -> integer datatype declaration
+display -> command for printing to terminal (see displaying to terminal section)
+
 
 Current Supported Datatypes:
 int (integer)
@@ -29,7 +38,7 @@ str (string)
 bol (boolean)
 
 Currently Supported Operators:
-=
+=, :var: (to append to literal), 
 
 Planned Supported Operators: 
 +, -, /, *, %, ++, +=, --, 
